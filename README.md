b5b35a2ca61c40748c709269e32f1ba5b8466cbb
Wang Yiping <wangyiping@4paradigm.com>
2019-10-23 15:22:59 +0800

    feat: mysql 8.0.16

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-这个镜像关闭了explicit_defaults_for_timestamp=off
 
-镜像地址：docker-search.4pd.io/mysql-explicit-off:8.0.16
+这个镜像是用的默认的explicit_defaults_for_timestamp=on
+
+镜像地址：docker-search.4pd.io/mysql:8.0.16
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t docker02:35000/mysql-explicit-off:8.0.16 .
-docker push docker02:35000/mysql-explicit-off:8.0.16
+docker build -t docker02:35000/mysql:8.0.16 .
+docker push docker02:35000/mysql:8.0.16
